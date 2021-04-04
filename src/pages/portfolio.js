03af684aabf0fd0ba4b6d273da60e9752ea2a337
Thoko <thokozile.soko@yahoo.com>
2021-04-04 19:44:13 -0400

    I'm trying to change the project cards. I'm going to make a js file with one card and have it rcreate cards based on the projects.json file.

---
+++ src/pages/portfolio.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ProjectCard from "../components/Project/projectcard"
+import Navbar from "../components/Navbar/navbar"
+
+function Portfolio (){
+    return (
+        <div>
+            <Navbar />
+            <ProjectCard />
+        </div>
+    )
+}
+
+export default Portfolio;
