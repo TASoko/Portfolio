e49d1c7fee2d019315a925343a0752da79516c0b
Thoko <thokozile.soko@yahoo.com>
2021-04-04 17:28:27 -0400

    I made a navbar. Also the connection between componenets work.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,32 +1,20 @@
 import React from "react";
 
+
 function Navbar (){
     return (
       <div className="container">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <div className="container-fluid">
             <a className="navbar-brand" href="#">
-              Navbar
+              About
             </a>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Features
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Pricing
-                  </a>
-                </li>
-              </ul>
-            </div>
+            <a className="navbar-brand" href="#">
+              Portfolio
+            </a>
+            <a className="navbar-brand" href="#">
+              Contact
+            </a>
           </div>
         </nav>
       </div>
