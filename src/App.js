fb71502a1910a73fb93fb5623fcc39f51806639f
Thoko <thokozile.soko@yahoo.com>
2021-04-04 20:52:44 -0400

    I've added Routers to the app.js. I'm going to use them to get the navbar to switch between pages. Currently it's all on one page.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from "./pages/about";
 import "bootstrap/dist/css/bootstrap.min.css";
 import projects from "./projects.json";
 import Portfolio from "./pages/portfolio";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 
 class App extends Component {
@@ -12,19 +13,21 @@ class App extends Component {
   };
   render () {
   return (
-    <div className="main">
-      {/* <About /> */}
-      {this.state.projects.map((project) => (
-        <Portfolio
-          id={project.id}
-          key={project.id}
-          name={project.name}
-          media={project.media}
-          description={project.description}
-          languages={project.languages}
-        />
-      ))}
-    </div>
+    <Router>
+      <div className="main">
+        {/* <About /> */}
+        {this.state.projects.map((project) => (
+          <Portfolio
+            id={project.id}
+            key={project.id}
+            name={project.name}
+            media={project.media}
+            description={project.description}
+            languages={project.languages}
+          />
+        ))}
+      </div>
+    </Router>
   );
   }
 }
